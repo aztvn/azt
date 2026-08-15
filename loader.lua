1c7a4de29e863c1af042cd5bdf29c342a568db30
AZT Service <tranthiencpz@gmail.com>
2026-08-15 08:10:06 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,34 @@
+-- [[ Code Hệ Thống Key System & Loader ]]
+-- Made By GPT : https://discord.gg/236caWdzUY
+-- Don't Skid Skid
+-- Read readme.md
+
+return function(Data)
+
+    if Data.KeyStatus == "Success" then
+        Passed = true -- Kiểm Tra Key Thành Công Chạy Script
+    elseif Data.KeyStatus == "Warn" then
+        Passed = "Error" -- Kiểm Tra Key Thất Bại
+    else
+       Passed = "Hooked" -- Key Đã Bị Hook
+    end}
+    -- // Bỏ Id Gane Và Script Vào 
+    local ScriptId = {
+        [PlaceId] = "https://" 
+    }
+
+    -- Logic Loader
+    local _Loader = function(PlaceId)
+        if ScriptId[PlaceId] then
+            loadstring(game:HttpGet(ScriptId[PlaceId]))(Data)
+        end
+    end
+    local GetGameReturnID = function()
+        return tostring(game.PlaceId)
+    end
+    if Passed == "Success" then
+    _Loader(tostring(game.PlaceId)) -- Người Chơi Đã Vượt qua Được Auth
+    elseif Passed == "Warn" or Passed == "Hooked" then
+        return game:Shutdown() -- Đóng Game
+    end
+end
