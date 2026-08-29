208ceab97c6c02d272677e3a24f4133f9508648f
AZT Service <tranthiencpz@gmail.com>
2026-08-29 11:45:38 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -54,7 +54,7 @@ return function(Data)
         },
 
         ["17541114784"] = {
-            Url = "https://aztvn.top/azt/api/v2/cdvn.lua",
+            Url = "https://raw.githubusercontent.com/aztvn/azt/refs/heads/main/script/cdvn.lua",
             NameGame = "Cong Dong Viet Nam",
             BlacklistExec = {
                 "xeno",
